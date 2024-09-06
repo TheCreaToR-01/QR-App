7101fa2837a538e9700be280366d7057a642d21a
Kushagra Dubey <greatnamokushagra@gmail.com>
2024-09-06 11:48:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 // Frontend
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/public/index.html");
+    res.sendFile(__dirname + "/index.html");
 })
 
 app.post("/submit", (req, res) => {
